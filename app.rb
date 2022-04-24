1b212575cd48433d576892d779966e35dc1fd15c
storm-rothoff-nti-johanneberg <74712840+storm-rothoff-nti-johanneberg@users.noreply.github.com>
2022-04-24 23:28:33 +0200

    Alpha 1.2
    Inlämningsdags! Det som återstår att göra är korrekta errormedelanden och RESCUE-system vid bl.a ogilitg filuppladdning och kontoskapande.

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ require 'sqlite3'
 require 'bcrypt'
 
 enable :sessions
-
 def dbCalled(path)
     db = SQLite3::Database.new(path)
     db.results_as_hash = true
+    p db
     return db
 end
 
@@ -20,9 +20,11 @@ end
 get('/showregister') do
     slim(:register)
 end
+
 get('/showlogin') do
     slim(:login)
 end
+
 get('/showaccount/:id') do
     db = dbCalled("db/main.db")
     id = params[:id].to_i
@@ -71,15 +73,15 @@ post('/login') do
     end
 end
 
+
 post("/users/new") do
     username = params[:username]
     password = params[:password]
     password_confirm = params[:password_confirm]
-
     if password == password_confirm
         db = dbCalled('db/main.db')
         password_digest = BCrypt::Password.create(password)
-        db.execute("INSERT INTO users (username,pwddigest) VALUES (?,?)",username,password_digest)
+        db.execute("INSERT INTO users (username,pwddigest,tokens) VALUES (?,?,?)",username,password_digest,100)
         redirect('/')
     else
         "Passwords do not match"
@@ -118,16 +120,27 @@ end
 
 post("/image/new") do 
     id = session[:id].to_i
+    nameMaxLength = params[:imageName][0..30]
     path = File.join("./public/img/",params[:imageFile]["filename"])
     pathForDb = File.join("img/",params[:imageFile]["filename"])
-    db = dbCalled('db/main.db')
-    mod = rand(1...10)  
-    db.execute("INSERT INTO images (path, mod, name, user_id, creator_id) VALUES (?, ?, ?, ?, ?)", pathForDb, mod, params[:imageName], id, id)
+    pathForExtention = path.delete_prefix('.')    
 
-    File.open(path, 'wb') do |f|
-        f.write(params[:imageFile][:tempfile].read)
-    end      
-    redirect('/images')
+    if session[:id] == nil
+        p "Not logged in"
+        redirect('/images')
+    elsif  File.extname(pathForExtention) == ".png" || File.extname(pathForExtention) == ".jpg" 
+        db = dbCalled('db/main.db')
+        mod = rand(1...10)  
+        db.execute("INSERT INTO images (path, mod, name, user_id, creator_id) VALUES (?, ?, ?, ?, ?)", pathForDb, mod, params[:imageName], id, id)
+        
+        File.open(path, 'wb') do |f|
+            f.write(params[:imageFile][:tempfile].read)
+        end
+        redirect('/images')
+    else
+        p "Incorrect File Input"
+        redirect('/images')
+    end
 end
 
 post('/images/show/delete/:id') do 
@@ -153,21 +166,16 @@ post('/images/show/purchase/:id') do
     user_id = session[:id].to_i
     # usersUnsorted = db.execute("SELECT id, username FROM users")
     # users = usersUnsorted.sort_by { |k| k["id"] }
-   
-   
+    #Logik
     seller_id = db.execute("SELECT user_id FROM images WHERE id = ?", id)
-    p seller_id
     price = db.execute("SELECT price FROM images WHERE id = ?", id)
     tokensBuyer = db.execute("SELECT tokens FROM users WHERE id = ?", user_id)
     tokensSeller = db.execute("SELECT tokens FROM users WHERE id = ?", seller_id[0]["user_id"])
     newBuyer = tokensBuyer[0]["tokens"].to_i - price[0]["price"].to_i
     newSeller = tokensSeller[0]["tokens"].to_i + price[0]["price"].to_i
-
-
-
+    #Skriver
     db.execute("UPDATE users SET tokens = ? WHERE id = ?", newBuyer, user_id)
     db.execute("UPDATE users SET tokens = ? WHERE id = ?", newSeller, seller_id[0]["user_id"])
-
     db.execute("UPDATE images SET user_id = ? WHERE id = ?", user_id, id)
     session[:tokens] = db.execute("SELECT tokens FROM users WHERE id = ?", user_id)[0]["tokens"]
     redirect("/images/show/#{id}")
